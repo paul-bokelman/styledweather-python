5caa57509d33f2e6e4f08dbdd4fe178897b5d983
paulb <paul.bokelman1@gmail.com>
2021-04-02 11:31:14 -0700

    mini lab

--- paul.py
+++ paul.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
 paul = Blueprint('paul', __name__)
 
+class Info:
+    name = "Paul"
+    lang = "javascript"
+    desc="I am Paul Bokelman and I really enjoy coding and making cool projects on the internet and solving problems. I also have a very adorable dog."
+    github="https://github.com/Paul-Bokelman"
+    
+info = Info()
 
 @paul.route("/paul")
 def paul_route():
-    return render_template("indiviual.html", name="Paul", lang="javascript", desc="I am Paul Bokelman and I really enjoy coding and making cool projects on the internet and solving problems. I also have a very adorable dog.")
+    return render_template("indiviual.html", name=info.name, lang=info.lang, desc=info.desc, github=info.github)
