a399e87702aee0e62e3f850a5c8bedbd54cae49c
samkoenig9 <samkoenig100@gmail.com>
2021-04-02 22:25:36 -0700

    sam.py

--- travis.py
+++ travis.py
@@ -7,7 +7,6 @@ class Info:
     desc="My name is Travis Medley and I am a triplet. I Love to code in CSS and currently computer science is my favorite class because I have freedom to do creative things. When I'm bored I love to play video games with friends and now that I understand more code, we try to sometimes understand the code of the game."
     github="https://github.com/Travis4th"
 
-    
 info = Info()
 
 @travis.route("/travis")
