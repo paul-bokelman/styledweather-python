28e2ea20cfdcea9306597a526067a33987e22270
Paul Bokelman <72945168+Paul-Bokelman@users.noreply.github.com>
2021-04-06 17:51:14 -0700

    alg

--- site_routes.py
+++ site_routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import math 
 site = Blueprint('site', __name__)
-
 
 @site.route("/secret")
 def secret():
     return render_template("secret.html")
+
+@site.route("/alg", methods=['GET','POST'])
+def alg():
+    a = int(request.form.get("a", False))
+    b = int(request.form.get("b", False))
+    c_squared = a**2 + b**2
+    c = math.sqrt(c_squared)
+    return render_template("alg.html", output=c)
