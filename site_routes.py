4337674609c5aa681226fd5227b7a2a0b4f1bbf3
samkoenig9 <samkoenig100@gmail.com>
2021-04-07 12:59:07 -0700

    cylinder math

--- site_routes.py
+++ site_routes.py
@@ -16,3 +16,11 @@ def alg():
     c_squared = a**2 + b**2
     c = math.sqrt(c_squared)
     return render_template("alg.html", output=c)
+
+@site.route("/cylinder", methods=['GET','POST'])
+def cylinder():
+    r = int(request.form.get("r", False))
+    h = int(request.form.get("h", False))
+    r_squared = r**2
+    v = round(math.pi*r_squared*h,2)
+    return render_template("cylinder.html", output=v)
