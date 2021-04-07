ec769bbd9a5063b556dece8d9362c4b45b262d3c
paulb <paul.bokelman1@gmail.com>
2021-04-07 12:20:46 -0700

    Update site_routes.py

--- site_routes.py
+++ site_routes.py
@@ -15,5 +15,4 @@ def alg():
     b = int(request.form.get("b", False))
     c_squared = a**2 + b**2
     c = math.sqrt(c_squared)
-    new = math.sqrt(-5)
-    return render_template("alg.html", output=c, new=new)
+    return render_template("alg.html", output=c)
