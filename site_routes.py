4c5d92762c204c853c35dc8c67c11c4015185341
paulb <paul.bokelman1@gmail.com>
2021-04-21 13:49:50 -0700

    alg

--- site_routes.py
+++ site_routes.py
@@ -11,11 +11,30 @@ def secret():
 
 @site.route("/alg", methods=['GET','POST'])
 def alg():
-    a = int(request.form.get("a", False))
-    b = int(request.form.get("b", False))
-    c_squared = a**2 + b**2
-    c = math.sqrt(c_squared)
-    return render_template("alg.html", output=c)
+    class pathAlg: 
+        def __init__(self, a = 0, b = 0, c = 0):
+            self._a = a
+            self._b = b
+            self._c = c
+
+        def get_final(self):
+            a = self._a
+            b = self._b
+            c = a**2 + b**2
+            self._c = math.sqrt(c)
+            return [a, b, self._c]
+
+        def set_final(self, a, b):
+            self._a = a
+            self._b = b
+
+    path = pathAlg()
+    path.set_final(int(request.form.get("a", 2)), int(request.form.get("b", 2)))
+    value_array = path.get_final()
+    a = value_array[0]
+    b = value_array[1]
+    c = value_array[2]
+    return render_template("alg.html", a=a, b=b, output=c)
 
 @site.route("/cylinder", methods=['GET','POST'])
 def cylinder():
