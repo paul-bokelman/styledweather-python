7e8120938e759ad9408f1952bfa5dcdfc7152a62
samkoenig9 <samkoenig100@gmail.com>
2021-04-23 11:13:46 -0700

    cylinder calc thing

--- site_routes.py
+++ site_routes.py
@@ -38,8 +38,26 @@ def alg():
 
 @site.route("/cylinder", methods=['GET','POST'])
 def cylinder():
-    r = int(request.form.get("r", False))
-    h = int(request.form.get("h", False))
-    r_squared = r**2
-    v = round(math.pi*r_squared*h,2)
-    return render_template("cylinder.html", output=v)
+    class pathCylinder: 
+        def __init__(self, r = 0, h = 0, v = 0):
+            self._r = r
+            self._h = h
+            self._v = v
+
+        def get_final(self):
+            r = self._r
+            h = self._h
+            self._v = round(math.pi*(r**2)*h,2)
+            return [r, h, self._v]
+
+        def set_final(self, r, h):
+            self._r = r
+            self._h = h
+
+    path = pathCylinder()
+    path.set_final(int(request.form.get("r", 2)), int(request.form.get("h", 2)))
+    value_array = path.get_final()
+    r = value_array[0]
+    h = value_array[1]
+    v = value_array[2]
+    return render_template("cylinder.html", r=r, h=h, output=v)
