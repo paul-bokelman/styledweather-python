1940f0e3123611896877abe1d8783980a5d8e480
paulb <paul.bokelman1@gmail.com>
2021-04-26 00:28:14 -0700

    bubble

--- paul.py
+++ paul.py
@@ -11,4 +11,19 @@ info = Info()
 
 @paul.route("/paul")
 def paul_route():
+    class bubbleSort:
+        def __init__(self):
+            self._list = []
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    bubbleSort(arr)
+    print ("Sorted array is:")
+    for i in range(len(arr)):
+        print ("%d" %arr[i])
+
     return render_template("indiviual.html", name=info.name, lang=info.lang, desc=info.desc, github=info.github)
