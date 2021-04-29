9c533218c813228a7af3a9febf818e734cf3803b
samkoenig9 <samkoenig100@gmail.com>
2021-04-29 16:20:31 -0700

    bubble

--- sam.py
+++ sam.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 sam = Blueprint('sam', __name__)
 
 class Info:
@@ -9,6 +9,23 @@ class Info:
     
 info = Info()
 
-@sam.route("/sam")
+@sam.route("/sam", methods=['GET', 'POST'])
 def sam_route():
-    return render_template("indiviual.html", name=info.name, lang=info.lang, desc=info.desc, github=info.github)
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    original = request.form.getlist('input_text[]')
+    arr = [1, 2]
+    if request.method != 'GET':
+        if original[0].isnumeric():
+            arr = [int(i) for i in original]
+        else:
+            arr = original
+        bubbleSort(arr)
+    
+
+    return render_template("indiviual.html", name=info.name, lang=info.lang, desc=info.desc, github=info.github, original=original, sorted=arr, paul=False)
