17dee0b243b7cd0c45f26a469463febf2e03c078
paulb <paul.bokelman1@gmail.com>
2021-05-14 20:14:01 -0700

    completed ticket
    - Created auth system
    - Created relational database between User and IdealWeather
    - Added login, signup, and dashboard templates
    - Added minor HTML functionality
    - Finishing full functionality

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-from utils import RegistrationForm, LoginForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from datetime import datetime
 
 from routes.paul import about_paul
@@ -26,6 +30,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 csrf = CSRFProtect(app)
 csrf.init_app(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 
 app.register_blueprint(about_paul.about_paul)
 app.register_blueprint(paul_bubblesort.paul_bubblesort)
@@ -45,12 +53,12 @@ app.register_blueprint(api)
 app.register_blueprint(junction)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model): 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    ideal_weather = db.relationship('IdealWeather', backref='author', lazy=True)
+    username = db.Column(db.String(15), unique=True) 
+    email = db.Column(db.String(50), unique=True) 
+    password = db.Column(db.String(80)) 
+    ideal_weather = db.relationship('IdealWeather', backref='author')
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -67,26 +75,82 @@ class IdealWeather(db.Model):
     def __repr__(self):
         return f"IdealWeather('{self.condition}', '{self.temp}', '{self.desc}', '{self.date_added}')"
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('api.home'))
-    return render_template('register.html', title='Register', form=form)
 
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
-@app.route("/login", methods=['GET', 'POST'])
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('api.home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+	form = LoginForm()
 
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data): 
+				login_user(user, remember=form.remember.data)
+				return redirect(url_for('dashboard'))
+
+		return redirect(url_for('login'))
+
+	return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = RegisterForm()
+
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method='sha256')
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+		# redirect to page when user is created
+		return redirect(url_for('login'))
+		# return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+	return render_template('signup.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    record = False
+    user = User.query.filter_by(username=current_user.username).first()
+    user_weather = user.ideal_weather
+    if user_weather:
+        condition = user_weather[0].condition
+        temp = user_weather[0].temp
+        desc =user_weather[0].desc
+        date =user_weather[0].date_added
+        record = True
+        return render_template('dashboard.html', name=current_user.username, condition=condition, temp=temp, desc=desc, date=date, record=record)
+    else:
+        return render_template('dashboard.html', name=current_user.username, record=record)
+
+@app.route('/handle_data', methods=['GET','POST'])
+def handle_data(): 
+    username = User.query.filter_by(username=current_user.username).first()
+    condition = request.form.get("condition", 'sunny')
+    temp = int(request.form.get("temp", '72'))
+    desc = request.form.get("desc", 'A nice sunny day with no distractions.')
+    iw = IdealWeather(condition=condition, temp=temp, desc=desc, author=username)
+    db.session.add(iw)
+    db.session.commit()
+    return redirect(url_for('dashboard'))
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('api.home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
